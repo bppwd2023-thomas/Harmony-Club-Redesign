cc5eab34ccb1c5e90f34d0b02ea5366e0259b3b3
Jacob Thomas <ja.thomas@baypath.net>
2023-01-17 18:51:16 +0000

    did work yk

--- hcow/config/routes.rb
+++ hcow/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'site/home'
   resources :sponsors
   resources :photos
   resources :albums
@@ -8,5 +10,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root "site#home"
 end
