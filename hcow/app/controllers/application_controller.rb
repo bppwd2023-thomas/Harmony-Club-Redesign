a9ab3173af3ef141952078c9672189512d626d45
Jacob Thomas <ja.thomas@baypath.net>
2023-01-18 15:48:24 +0000

    added styling to home page and login

--- hcow/app/controllers/application_controller.rb
+++ hcow/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+ @current_year = DateTime.now.year
+ puts "\n\n\n\n\n\n\n\n\n\n\n\n\n"
+ puts @current_year
+ puts "\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
 end
