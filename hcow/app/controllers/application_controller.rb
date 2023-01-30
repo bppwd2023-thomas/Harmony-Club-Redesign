c22f3b7d63c5bb0977e1600e2e00a5ad44333d7b
Jacob Thomas <ja.thomas@baypath.net>
2023-01-30 19:20:36 +0000

    dropdown navbar

--- hcow/app/controllers/application_controller.rb
+++ hcow/app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
 
  @current_year = DateTime.now.year
+
+ protected
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+ end
 
 end
