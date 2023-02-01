6423337d7a354c6ea2a7b6ebf52d17724cce64cc
Jacob Thomas <ja.thomas@baypath.net>
2023-02-01 17:20:09 +0000

    stimulus stuff

---
+++ hcow/app/javascript/controllers/page_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "select", "destination", "content", "fileSelector" ]
+
+  toggle() {
+   if(this.selectTarget.value == "Basic Page") {
+
+     this.contentTarget.style.display = "block";
+     this.destinationTarget.style.display = "none";
+     this.fileSelectorTarget.style.display = "none";
+
+   }else if(this.selectTarget.value == "Internal Link") {
+
+     this.contentTarget.style.display = "none";
+     this.destinationTarget.style.display = "block";
+     this.fileSelectorTarget.style.display = "none";
+
+   }else if(this.selectTarget.value == "External Link") {
+
+     this.contentTarget.style.display = "none";
+     this.destinationTarget.style.display = "block";
+     this.fileSelectorTarget.style.display = "none";
+
+   }else if(this.selectTarget.value == "PDF") {
+
+     this.contentTarget.style.display = "none";
+     this.destinationTarget.style.display = "none";
+     this.fileSelectorTarget.style.display = "block";
+
+   }
+  }
+}
